925a0dcbb95f20d1318a391c133e9d5ec67d4363
jumbomuffin101 <ryanrawat@gmail.com>
2025-10-17 14:19:50 -0400

    feat: add netlify function and config

---
+++ frontend/netlify/functions/api/api.py
@@ -0,0 +1,11 @@
+﻿from fastapi import FastAPI
+from mangum import Mangum
+
+app = FastAPI(title="Netlify FastAPI")
+
+@app.get("/ping")
+def ping():
+    return {"message": "pong"}
+
+# Netlify/AWS Lambda looks for 'handler'
+handler = Mangum(app)
