81eb129f3165d56459666f36fc7be847827d83eb
Aryan Rawat <your-email@example.com>
2025-10-30 12:51:29 -0400

    Updated name

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/ssmif-quant-dev/",   // must match the REPO name with leading+trailing slashes
+  base: "/Trading-Analytics-Dashboard/",   // must match the REPO name with leading+trailing slashes
   build: { outDir: "dist" }
 });
